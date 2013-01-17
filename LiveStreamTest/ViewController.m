6d60cc24068b67f9fd366dbc01f537994e787c04
Nelson <nelson@Nelsons-Mac-mini.local>
2013-01-17 13:21:16 +0800

    live stream

--- LiveStreamTest/ViewController.m
+++ LiveStreamTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface ViewController ()
 
@@ -14,16 +15,45 @@
 
 @implementation ViewController
 
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    
+    MPMoviePlayerViewController *player = [[MPMoviePlayerViewController alloc] initWithContentURL:[NSURL URLWithString:@"http://54.248.230.82:1935/live/demo700k/playlist.m3u8"]];
+    
+    player.moviePlayer.movieSourceType = MPMovieSourceTypeStreaming;
+    
+    [self presentMoviePlayerViewControllerAnimated:player];
+     
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(logm3u8) userInfo:nil repeats:YES];
+    
+
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)logm3u8
+{
+    NSError *error;
+    NSString *str = [NSString stringWithContentsOfURL:[NSURL URLWithString:@"http://54.248.230.82:1935/live/demo700k/playlist.m3u8"] encoding:NSUTF8StringEncoding error:&error];
+    
+    NSLog(@"error:%@", error);
+    
+    NSLog(@"m3u8:\n\n%@\n\n\n", str);
 }
 
 @end
